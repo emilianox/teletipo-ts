1381232fc0f41f40f16febc94541bc959a95bffb
Emiliano Fernandez <emiliano@hashdog.com>
2016-10-23 11:34:21 -0300

    remove _js folder

--- _js/test/test.js
+++
@@ -1,9 +0,0 @@
-function testStartCommand() {
-    return testDoPostCommand_("/start");
-}
-function testOnlySudoCommand() {
-    return testDoPostCommand_("/onlySudo");
-}
-function testReviewCommand() {
-    return testDoPostCommand_("/review");
-}
