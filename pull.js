727f5e27f7a655bc1d2b53a83ad9504d4a9cde34
Emiliano Fernandez <emiliano@hashdog.com>
2016-10-23 11:36:40 -0300

    fixed upload scripts!

--- pull.js
+++ pull.js
@@ -1,24 +1,61 @@
-var config, exec, print, process;
-config = require('./_js/configs/_config.js');
-process = require('process');
-exec = require('child_process').exec;
-print = function(error, stdout, stderr) {
-  return console.log(stdout);
-};
 
-console.log("#####BEGIN SCRIPT#######");
+var process = require('process')
+var path = require('path')
+var exec = require('child_process').exec
 
-process.chdir(__dirname);
+var srcFolder = path.join(__dirname, 'src')
+var configTsFile = path.join(srcFolder, 'configs', '_config.ts')
 
-if (config.CONSTANTS.googleAppScript.project_name) {
-  exec("rm -rf " + __dirname + "/" + config.CONSTANTS.googleAppScript.project_name, function(error, stdout, stderr) {
-    console.log("clonning from gapps");
-    return exec("gaps clone " + config.CONSTANTS.googleAppScript.id_gapps, function(error, stdout, stderr) {
-      console.log(stdout);
-      console.log("Compiling...");
-      return exec("rm -rf " + __dirname + "/" + config.CONSTANTS.googleAppScript.project_name + "/*.js", function(error, stdout, stderr) {});
-    });
-  });
-} else {
-  console.log("Please add CONSTANTS.googleAppScript.project_name in _configs.coffee");
-}
+exec('tsc ' + configTsFile + ' --outDir .', function (error, stdout, stderr) {
+  if (error) {
+    console.log('_config.ts compilation failed')
+  }
+  var config = require(path.join(__dirname, '_config.js'))
+
+  if (config.CONSTANTS.googleAppScript.project_name) {
+    console.info('#####BEGIN SCRIPT#######')
+    // Folders
+    var projectFolder = path.join(__dirname, config.CONSTANTS.googleAppScript.project_name)
+    var jsInProjectFolder = path.join(projectFolder, '*.js')
+    // Commands
+    var removeProjectFolder = 'rm -rf ' + projectFolder
+    var cloneGaps = 'gaps clone ' + config.CONSTANTS.googleAppScript.id_gapps
+    var removeAllJsInProjectFolder = 'rm -rf ' + jsInProjectFolder
+    var removeConfig = 'rm ' + path.join(__dirname, '_config.js')
+    // Move to project
+    process.chdir(__dirname)
+    // Remove old files
+    exec(removeProjectFolder, function (error, stdout, stderr) {
+      if (error) {
+        console.error('remove files failed')
+        return false
+      }
+      console.info('Clonning from gapps')
+      // Get info from gaps
+      exec(cloneGaps, function (error, stdout, stderr) {
+        if (error) {
+          console.error('gaps clone failed')
+          return false
+        }
+        console.info(stdout)
+        // Remove Config
+        exec(removeConfig, function (error, stdout, stderr) {
+          if (error) {
+            console.error('remove _config.js failed')
+            return false
+          }
+        })
+        // Remove all js Files(keep manifest.json)
+        exec(removeAllJsInProjectFolder, function (error, stdout, stderr) {
+          if (error) {
+            console.error('remove all js compiled failed')
+            return false
+          }
+        })
+      })
+    })
+  } else {
+    console.error('Please add CONSTANTS.googleAppScript.project_name in _configs.coffee')
+    return false
+  }
+})
--- push.js
+++ push.js
@@ -1,19 +1,53 @@
-var config, exec, print, process;
+var process = require('process')
+var path = require('path')
+var exec = require('child_process').exec
 
-config = require('./_js/configs/_config.js');
+var srcFolder = path.join(__dirname, 'src')
+var configTsFile = path.join(srcFolder, 'configs', '_config.ts')
 
-process = require('process');
+exec('tsc ' + configTsFile + ' --outDir .', function (error, stdout, stderr) {
+  if (error) {
+    console.log('_config.ts compilation failed')
+  }
+  var config = require(path.join(__dirname, '_config.js'))
 
-exec = require('child_process').exec;
-
-print = function(error, stdout, stderr) {
-  return console.log(stdout);
-};
-
-console.log("#####BEGIN SCRIPT#######");
-
-process.chdir(__dirname + "/" + config.CONSTANTS.googleAppScript.project_name);
-
-exec("tsc --sourceRoot ../src --outDir .", function(error, stdout, stderr) {
-  return exec("gaps push", print);
-});
+  if (config.CONSTANTS.googleAppScript.project_name) {
+    console.info('#####BEGIN SCRIPT#######')
+    // Folders
+    var projectFolder = path.join(__dirname, config.CONSTANTS.googleAppScript.project_name)
+    // Commands
+    var compileTs = 'tsc --sourceMap --sourceRoot ' + srcFolder + ' --outDir ' + projectFolder
+    var pushGaps = 'gaps push'
+    var removeConfig = 'rm ' + path.join(__dirname, '_config.js')
+    // Compile ts files
+    exec(compileTs, function (error, stdout, stderr) {
+      if (error) {
+        console.error('compile failed')
+        console.error(error, stdout, stderr)
+        return false
+      }
+      console.info('Pushing to gapps')
+      // Move to project
+      process.chdir(projectFolder)
+      // Push script to gaps
+      exec(pushGaps, function (error, stdout, stderr) {
+        if (error) {
+          console.error('gaps push failed')
+          console.error(error, stdout, stderr)
+          return false
+        }
+        console.info(stdout)
+        // Remove Config
+        exec(removeConfig, function (error, stdout, stderr) {
+          if (error) {
+            console.error('remove _config.js failed')
+            return false
+          }
+        })
+      })
+    })
+  } else {
+    console.error('Please add CONSTANTS.googleAppScript.project_name in _configs.coffee')
+    return false
+  }
+})
